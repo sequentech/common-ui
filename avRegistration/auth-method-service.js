747ae8a6b8f5ed106ecee81760e0512831fb8c19
Findeton <felix@nvotes.com>
2017-08-31 13:41:36 +0200

    add extra functions to authmethod

--- avRegistration/auth-method-service.js
+++ avRegistration/auth-method-service.js
@@ -58,6 +58,38 @@ angular.module('avRegistration')
             var eid = authevent || authId;
             return $http.post(backendUrl + 'auth-event/'+eid+'/register/', data);
         };
+        
+        authmethod.getUserInfoExtra = function() {
+            if (!authmethod.isLoggedIn()) {
+              var data = {
+                success: function () { return data; },
+                error: function (func) {
+                  setTimeout(function() {
+                    func({message:"not-logged-in"});
+                  }, 0);
+                  return data;
+                }
+              };
+              return data;
+            }
+            return $http.get(backendUrl + 'user/extra/', {});
+        };
+
+        authmethod.updateUserExtra = function (extra) {
+            if (!authmethod.isLoggedIn()) {
+              var data = {
+                success: function () { return data; },
+                error: function (func) {
+                  setTimeout(function() {
+                    func({message:"not-logged-in"});
+                  }, 0);
+                  return data;
+                }
+              };
+              return data;
+            }
+            return $http.post(backendUrl + 'user/extra/', extra);
+        };
 
         authmethod.getUserInfo = function(userid) {
             if (!authmethod.isLoggedIn()) {
