1df38ecd03ac8ec36a547b7a11f8e8f9fd6a158a
Eduardo Robles Elvira <edulix@nvotes.com>
2018-02-08 09:33:40 +0100

    fixing typo

--- avRegistration/auth-method-service.js
+++ avRegistration/auth-method-service.js
@@ -78,7 +78,7 @@ angular.module('avRegistration')
         /**
          * @returns an activity page
          */
-        authmethod.getActivity = function(eid, page, size, filterOptions, filteredStr, receiver_id)
+        authmethod.getActivity = function(eid, page, size, filterOptions, filterStr, receiver_id)
         {
             var params = {};
             var url = backendUrl + 'auth-event/' + eid + '/activity/';
