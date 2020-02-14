cd1976ef97973e3439fa3b6c87f9b420e8bf9118
Eduardo Robles Elvira <edulix@nvotes.com>
2020-02-14 11:30:49 +0100

    compiled

---
+++ dist/avWidgets.js
@@ -0,0 +1,32 @@
+!function() {
+    function createElement(name, attrs) {
+        var el = document.createElement(name);
+        for (var key in attrs) el.setAttribute(key, attrs[key]);
+        return el;
+    }
+    function processLinks(className, callback) {
+        for (var links = document.getElementsByClassName(className), i = 0; i < links.length; i++) {
+            var link = links[i], href = link.getAttribute("href");
+            void 0 !== callback && callback(link, i, className);
+            var iframe = createElement("iframe", {
+                class: className + "-iframe",
+                src: href,
+                style: "border: 0; width: 100%; height: 100%",
+                seamless: ""
+            });
+            link.parentNode.insertBefore(iframe, link), link.parentNode.removeChild(link);
+        }
+    }
+    window.addEventListener("message", function(e) {
+        var reqAuth = "avRequestAuthorization:";
+        if (e.data.substr(0, reqAuth.length) === reqAuth) {
+            var args = [ JSON.parse(e.data.substr(reqAuth.length, e.data.length)), function(khmac) {
+                e.source.postMessage("avPostAuthorization:" + khmac, "*");
+            } ];
+            window[window.avRequestAuthorizationFuncName].apply(window, args);
+        }
+    }, !1), processLinks("agoravoting-voting-booth", function(link) {
+        var funcName = link.getAttribute("data-authorization-funcname");
+        window.avRequestAuthorizationFuncName = funcName;
+    }), processLinks("agoravoting-ballot-locator"), processLinks("agoravoting-results");
+}();
