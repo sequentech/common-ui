b25d895c6eb9a4382e0b46629e0b745829270f9b
Eduardo Robles Elvira <edulix@nvotes.com>
2021-07-10 06:13:01 +0200

    removing dependency to angular-date-picker-polyfill

--- dist/appCommon-v20.2.0.js
+++ dist/appCommon-v20.2.0.js
@@ -1372,7 +1372,7 @@ angular.module("avRegistration").config(function() {}), angular.module("avRegist
         },
         templateUrl: "avUi/foot-directive/foot-directive.html"
     };
-} ]), angular.module("agora-gui-common", [ "ui.bootstrap", "ui.utils", "ui.router", "ngAnimate", "ngResource", "ngCookies", "ipCookie", "ngSanitize", "infinite-scroll", "angularMoment", "avConfig", "jm.i18next", "avRegistration", "avUi", "avTest", "angularFileUpload", "dndLists", "angularLoad", "angular-date-picker-polyfill", "ng-autofocus" ]), 
+} ]), angular.module("agora-gui-common", [ "ui.bootstrap", "ui.utils", "ui.router", "ngAnimate", "ngResource", "ngCookies", "ipCookie", "ngSanitize", "infinite-scroll", "angularMoment", "avConfig", "jm.i18next", "avRegistration", "avUi", "avTest", "angularFileUpload", "dndLists", "angularLoad", "ng-autofocus" ]), 
 angular.module("jm.i18next").config([ "$i18nextProvider", "ConfigServiceProvider", function($i18nextProvider, ConfigServiceProvider) {
     $("#no-js").hide(), $i18nextProvider.options = _.extend({
         useCookie: !0,
