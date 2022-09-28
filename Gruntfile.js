d101e9222f4c910898f56cb78389c7043f70cc6b
Findeton <felix@sequentech.io>
2022-09-28 10:04:32 -0500

    Release for version 7.0.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@
 'use strict';
 
 var pkg = require('./package.json');
-var SEQUENT_CONFIG_VERSION = '7.0.0-beta.1';
+var SEQUENT_CONFIG_VERSION = '7.0.0';
 
 //Using exclusion patterns slows down Grunt significantly
 //instead of creating a set of patterns like '**/*.js' and '!**/node_modules/**'
@@ -165,10 +165,10 @@ module.exports = function (grunt) {
         options: {
           remove: ['script[data-remove!="false"]','link[data-remove!="false"]'],
           append: [
-            {selector:'body',html:'<script src="/libnocompat-v7.0.0-beta.1.js"></script>'},
+            {selector:'body',html:'<script src="/libnocompat-v7.0.0.js"></script>'},
             {selector:'body',html:'<!--All the source code of this program under copyright. Take a look at the license details at https://github.com/sequent/sequent-core-common/blob/master/README.md -->'},
-            {selector:'body',html:'<script src="/appCommon-v7.0.0-beta.1.js"></script>'},
-            {selector:'body',html:'<script src="/SequentPlugins-v7.0.0-beta.1.js"></script>'},
+            {selector:'body',html:'<script src="/appCommon-v7.0.0.js"></script>'},
+            {selector:'body',html:'<script src="/SequentPlugins-v7.0.0.js"></script>'},
             {selector:'head',html:'<link rel="stylesheet" id="theme" href="/themes/default/app.min.css">'}
           ]
         },
@@ -194,9 +194,9 @@ module.exports = function (grunt) {
           'temp/libnocompat.js': ['<%= dom_munger.data.libnocompatjs %>'],
           'temp/lib.js': ['<%= dom_munger.data.libjs %>'],
           'temp/app.js': ['<%= dom_munger.data.appjs %>','<%= ngtemplates.main.dest %>'],
-          'dist/SequentConfig-v7.0.0-beta.1.js': ['SequentConfig.js'],
-          'dist/SequentThemes-v7.0.0-beta.1.js': ['SequentThemes.js'],
-          'dist/SequentPlugins-v7.0.0-beta.1.js': ['plugins/**/*.js']
+          'dist/SequentConfig-v7.0.0.js': ['SequentConfig.js'],
+          'dist/SequentThemes-v7.0.0.js': ['SequentThemes.js'],
+          'dist/SequentPlugins-v7.0.0.js': ['plugins/**/*.js']
         }
       }
     },
@@ -229,9 +229,9 @@ module.exports = function (grunt) {
           beautify: true
         },
         files: {
-          'dist/appCommon-v7.0.0-beta.1.js': 'temp/app.js',
-          'dist/libCommon-v7.0.0-beta.1.js': 'temp/lib.js',
-          'dist/libnocompat-v7.0.0-beta.1.js': 'temp/libnocompat.js',
+          'dist/appCommon-v7.0.0.js': 'temp/app.js',
+          'dist/libCommon-v7.0.0.js': 'temp/lib.js',
+          'dist/libnocompat-v7.0.0.js': 'temp/libnocompat.js',
           'dist/avWidgets.js': 'avWidgets.js',
 
           "dist/locales/moment/es.js": "node_modules/moment/locale/es.js",
