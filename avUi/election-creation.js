476b9682879933f12d6676a36c4319a417c04a00
Findeton <felix@sequentech.io>
2022-12-14 12:30:50 -0500

    wip

--- avUi/election-creation.js
+++ avUi/election-creation.js
@@ -21,7 +21,7 @@ angular.module('avUi')
         var service = {
         };
 
-        service.generateAutheventData = function (el) {
+        service.generateAuthapiRequest = function (el) {
             // sanitize some unneeded values that might still be there. This
             // needs to be done because how we use ng-model
             if (el.census.config.subject && !_.contains(['email', 'email-otp'], el.census.auth_method)) {
@@ -84,24 +84,77 @@ angular.module('avUi')
             return d;
         };
 
-        service.generateElectionData = function (el) {
-            if (typeof el.extra_data === 'object') {
-                el.extra_data = JSON.stringify(el.extra_data);
-            }
-            if (typeof el.tallyPipesConfig === 'object') {
-            el.tallyPipesConfig = JSON.stringify(el.tallyPipesConfig);
-            }
-            if (typeof el.ballotBoxesResultsConfig === 'object') {
-            el.ballotBoxesResultsConfig = JSON.stringify(el.ballotBoxesResultsConfig);
-            }
+        service.generateAuthapiResponse = function (el) {
+          var election = service.generateAuthapiRequest(el);
 
-            _.each(el.questions, function (q) {
-              _.each(q.answers, function (answer) {
-                answer.urls = _.filter(answer.urls, function(url) { return $.trim(url.url).length > 0;});
-              });
+          election.users = 0;
+          election.tally_status = "notstarted";
+          election.allow_public_census_query = false;
+          election.created = "2022-12-05T15:22:34.862203%2B00:00";
+          election.based_in = null;
+          election.hide_default_login_lookup_field = false;
+          election.auth_method_config.config = {
+            allow_user_resend: election.auth_method_config.allow_user_resend
+          };
+          election.openid_connect_providers = [];
+          election.inside_authenticate_otl_period = false;
+
+          return election;
+        };
+
+        service.generateBallotBoxRequest = function (data) {
+          var el = angular.copy(data);
+
+          if (typeof el.extra_data === 'object') {
+              el.extra_data = JSON.stringify(el.extra_data);
+          }
+          if (typeof el.tallyPipesConfig === 'object') {
+          el.tallyPipesConfig = JSON.stringify(el.tallyPipesConfig);
+          }
+          if (typeof el.ballotBoxesResultsConfig === 'object') {
+          el.ballotBoxesResultsConfig = JSON.stringify(el.ballotBoxesResultsConfig);
+          }
+
+          _.each(el.questions, function (q) {
+            _.each(q.answers, function (answer) {
+              answer.urls = _.filter(answer.urls, function(url) { return $.trim(url.url).length > 0;});
             });
+          });
 
-            return el;
+          return el;
+        };
+
+        service.generateBallotBoxResponse = function (el) {
+          var election = service.generateBallotBoxRequest(el);
+
+          election.ballotBoxesResultsConfig = "";
+          election.virtual = false;
+          election.tally_allowed = false;
+          election.publicCandidates = true;
+          election.virtualSubelections = [];
+          election.logo_url = "";
+
+          return {
+            id: election.id,
+            configuration: election,
+            state:"started",
+            // always use the same public keys
+            pks: JSON.stringify(election.questions.map(function (q) {
+              return {
+                q: '24792774508736884642868649594982829646677044143456685966902090450389126928108831401260556520412635107010557472033959413182721740344201744439332485685961403243832055703485006331622597516714353334475003356107214415133930521931501335636267863542365051534250347372371067531454567272385185891163945756520887249904654258635354225185183883072436706698802915430665330310171817147030511296815138402638418197652072758525915640803066679883309656829521003317945389314422254112846989412579196000319352105328237736727287933765675623872956765501985588170384171812463052893055840132089533980513123557770728491280124996262883108653723',
+                p: '49585549017473769285737299189965659293354088286913371933804180900778253856217662802521113040825270214021114944067918826365443480688403488878664971371922806487664111406970012663245195033428706668950006712214428830267861043863002671272535727084730103068500694744742135062909134544770371782327891513041774499809308517270708450370367766144873413397605830861330660620343634294061022593630276805276836395304145517051831281606133359766619313659042006635890778628844508225693978825158392000638704210656475473454575867531351247745913531003971176340768343624926105786111680264179067961026247115541456982560249992525766217307447',
+                y: '25233303610624276354982811986201834016697399044876854448496917180808794460600684041443897755355520203095802059616029587815193698920031231714345315925211168639624595654625128533802897292140868582328656520616332091010467955507834092620045939069623671407818190171090021825044623127204061232697474129851550188729946673890631720197446903235998242798036758238763406311552128366413931805575611209227161344639186615808279023879377699069225460149170905910146022296229949546176735955646970920639173343909852697354526383408023054713403757933275765703706664300550788437833505997522376371433614613665995482912523477014539823187236',
+                g: '27257469383433468307851821232336029008797963446516266868278476598991619799718416119050669032044861635977216445034054414149795443466616532657735624478207460577590891079795564114912418442396707864995938563067755479563850474870766067031326511471051504594777928264027177308453446787478587442663554203039337902473879502917292403539820877956251471612701203572143972352943753791062696757791667318486190154610777475721752749567975013100844032853600120195534259802017090281900264646220781224136443700521419393245058421718455034330177739612895494553069450438317893406027741045575821283411891535713793639123109933196544017309147'
+              };
+            })),
+            tallyPipesConfig: election.tallyPipesConfig,
+            ballotBoxesResultsConfig: "",
+            virtual: false,
+            tallyAllowed: false,
+            publicCandidates:true,
+            logo_url: "",
+            trusteeKeysState: []
+          };
         };
         
         return service;
--- dist/appCommon-vmaster.js
+++ dist/appCommon-vmaster.js
@@ -1406,7 +1406,7 @@ angular.module("avRegistration").config(function() {}), angular.module("avRegist
     return checker;
 }), angular.module("avUi").factory("ElectionCreation", function() {
     var service = {
-        generateAutheventData: function(el) {
+        generateAuthapiRequest: function(el) {
             el.census.config.subject && !_.contains([ "email", "email-otp" ], el.census.auth_method) && delete el.census.config.subject;
             var d = el.census.config["authentication-action"];
             "vote" === d.mode && (d["mode-config"] = null);
@@ -1433,7 +1433,17 @@ angular.module("avRegistration").config(function() {}), angular.module("avRegist
                 ef.max && (ef.max = parseInt(ef.max))), !0;
             }), d;
         },
-        generateElectionData: function(el) {
+        generateAuthapiResponse: function(election) {
+            election = service.generateAuthapiRequest(election);
+            return election.users = 0, election.tally_status = "notstarted", election.allow_public_census_query = !1, 
+            election.created = "2022-12-05T15:22:34.862203%2B00:00", election.based_in = null, 
+            election.hide_default_login_lookup_field = !1, election.auth_method_config.config = {
+                allow_user_resend: election.auth_method_config.allow_user_resend
+            }, election.openid_connect_providers = [], election.inside_authenticate_otl_period = !1, 
+            election;
+        },
+        generateBallotBoxRequest: function(el) {
+            el = angular.copy(el);
             return "object" == typeof el.extra_data && (el.extra_data = JSON.stringify(el.extra_data)), 
             "object" == typeof el.tallyPipesConfig && (el.tallyPipesConfig = JSON.stringify(el.tallyPipesConfig)), 
             "object" == typeof el.ballotBoxesResultsConfig && (el.ballotBoxesResultsConfig = JSON.stringify(el.ballotBoxesResultsConfig)), 
@@ -1444,6 +1454,31 @@ angular.module("avRegistration").config(function() {}), angular.module("avRegist
                     });
                 });
             }), el;
+        },
+        generateBallotBoxResponse: function(election) {
+            election = service.generateBallotBoxRequest(election);
+            return election.ballotBoxesResultsConfig = "", election.virtual = !1, election.tally_allowed = !1, 
+            election.publicCandidates = !0, election.virtualSubelections = [], election.logo_url = "", 
+            {
+                id: election.id,
+                configuration: election,
+                state: "started",
+                pks: JSON.stringify(election.questions.map(function(q) {
+                    return {
+                        q: "24792774508736884642868649594982829646677044143456685966902090450389126928108831401260556520412635107010557472033959413182721740344201744439332485685961403243832055703485006331622597516714353334475003356107214415133930521931501335636267863542365051534250347372371067531454567272385185891163945756520887249904654258635354225185183883072436706698802915430665330310171817147030511296815138402638418197652072758525915640803066679883309656829521003317945389314422254112846989412579196000319352105328237736727287933765675623872956765501985588170384171812463052893055840132089533980513123557770728491280124996262883108653723",
+                        p: "49585549017473769285737299189965659293354088286913371933804180900778253856217662802521113040825270214021114944067918826365443480688403488878664971371922806487664111406970012663245195033428706668950006712214428830267861043863002671272535727084730103068500694744742135062909134544770371782327891513041774499809308517270708450370367766144873413397605830861330660620343634294061022593630276805276836395304145517051831281606133359766619313659042006635890778628844508225693978825158392000638704210656475473454575867531351247745913531003971176340768343624926105786111680264179067961026247115541456982560249992525766217307447",
+                        y: "25233303610624276354982811986201834016697399044876854448496917180808794460600684041443897755355520203095802059616029587815193698920031231714345315925211168639624595654625128533802897292140868582328656520616332091010467955507834092620045939069623671407818190171090021825044623127204061232697474129851550188729946673890631720197446903235998242798036758238763406311552128366413931805575611209227161344639186615808279023879377699069225460149170905910146022296229949546176735955646970920639173343909852697354526383408023054713403757933275765703706664300550788437833505997522376371433614613665995482912523477014539823187236",
+                        g: "27257469383433468307851821232336029008797963446516266868278476598991619799718416119050669032044861635977216445034054414149795443466616532657735624478207460577590891079795564114912418442396707864995938563067755479563850474870766067031326511471051504594777928264027177308453446787478587442663554203039337902473879502917292403539820877956251471612701203572143972352943753791062696757791667318486190154610777475721752749567975013100844032853600120195534259802017090281900264646220781224136443700521419393245058421718455034330177739612895494553069450438317893406027741045575821283411891535713793639123109933196544017309147"
+                    };
+                })),
+                tallyPipesConfig: election.tallyPipesConfig,
+                ballotBoxesResultsConfig: "",
+                virtual: !1,
+                tallyAllowed: !1,
+                publicCandidates: !0,
+                logo_url: "",
+                trusteeKeysState: []
+            };
         }
     };
     return service;
