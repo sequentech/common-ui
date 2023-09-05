aa87f32bf47c7ddb6a3b1a007c9fdbbc613fadee
Eduardo Robles <edu@sequentech.io>
2023-09-05 08:30:48 +0200

    alternative auth methods: switch back to smartlink tab fails (#333)
    Parent issue: https://github.com/sequentech/meta/issues/222

--- avRegistration/login-directive/login-directive.js
+++ avRegistration/login-directive/login-directive.js
@@ -226,6 +226,9 @@ angular.module('avRegistration')
             return;
           }
           scope.authToken = $location.search()['auth-token'];
+          if (scope.authToken === undefined) {
+            return;
+          }
 
           var length = 'khmac:///'.length;
           var tails = scope.authToken.substr(length);
@@ -510,18 +513,27 @@ angular.module('avRegistration')
          * @param {*} altAuthMethod altAuthMethod object
          */
         scope.setCurrentAltAuthMethod = function(altAuthMethod) {
-          if (altAuthMethod === scope.current_alt_auth_method_id) {
-            return;
-          }
-
           var authevent = angular.copy(scope.base_authevent);
           if (altAuthMethod === null) {
             scope.current_alt_auth_method_id = null;
-          } else {
-            scope.current_alt_auth_method_id = altAuthMethod.id;
-            authevent.extra_fields = altAuthMethod.extra_fields;
-            authevent.auth_method = altAuthMethod.auth_method_name;
+            scope.apply(authevent);
+            return;
           }
+          if (altAuthMethod.id === scope.current_alt_auth_method_id) {
+            return;
+          }
+
+          // smart link cannot be enabled if it doesn't come from the url
+          if (
+            scope.selectedAltMethod !== 'smart-link' &&
+            altAuthMethod.auth_method_name === 'smart-link'
+          ) {
+            return;
+          }
+
+          scope.current_alt_auth_method_id = altAuthMethod.id;
+          authevent.extra_fields = altAuthMethod.extra_fields;
+          authevent.auth_method = altAuthMethod.auth_method_name;
           scope.apply(authevent);
         };
 
