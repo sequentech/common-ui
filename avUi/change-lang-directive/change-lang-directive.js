2dfa901918665189370fd9ce2054b18e6a66c189
Findeton <felix@sequentech.io>
2023-11-12 11:03:32 -0500

    wip

--- avUi/change-lang-directive/change-lang-directive.js
+++ avUi/change-lang-directive/change-lang-directive.js
@@ -50,7 +50,6 @@ angular.module('avUi')
       scope.changeLang = function(lang)
       {
         $i18next.options.lng = lang;
-        $i18next.options.useLocalStorage = true;
 
         // load i18n_overrides if any
         if (angular.isDefined($window.i18nOverride))
@@ -85,6 +84,7 @@ angular.module('avUi')
           .then(function () {
             amMoment.changeLocale(lang);
           });
+        $i18next.options.useLocalStorage = true;
       };
       scope.changeLang(scope.deflang);
 
