584c6ff0a65c6496c8562dc3ee68b25d9e85b737
Findeton <felix@sequentech.io>
2024-06-24 17:49:43 -0500

    wip

--- avRegistration/auth-method-service.js
+++ avRegistration/auth-method-service.js
@@ -371,7 +371,10 @@ angular.module('avRegistration')
             }
         };
 
-        authmethod.ping = function() {
+        authmethod.ping = function(pingId) {
+            if (!pingId) {
+              pingId = authId;
+            }
             if (!authmethod.isLoggedIn()) {
               var data = {
                 then: function (onSuccess, onError) {
@@ -383,7 +386,7 @@ angular.module('avRegistration')
               };
               return data;
             }
-            return $http.get(backendUrl + 'auth-event/'+authId+'/ping/');
+            return $http.get(backendUrl + 'auth-event/'+pingId+'/ping/');
         };
 
         authmethod.getImage = function(ev, uid) {
@@ -786,7 +789,7 @@ angular.module('avRegistration')
             return deferred.promise;
           }
           var now = Date.now();
-          return authmethod.ping()
+          return authmethod.ping(autheventid)
             .then(function(response) {
                 var options = {};
                 if (ConfigService.authTokenExpirationSeconds) {
